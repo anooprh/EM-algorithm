ad159953ad5f344d75cc10c557716f50ebeffd8a
anooprh <anoop.hallur@gmail.com>
2014-04-14 23:39:55 -0400

    Finally got it working

--- logmvnpdf.m
+++ logmvnpdf.m
@@ -12,35 +12,3 @@ end
 function y = logdet(A)
  y = log(det(A));
 end
-
-function y = logGaussPdf(X, mu, sigma)
-    % Compute log pdf of a Gaussian distribution.
-    % Written by Mo Chen (mochen80@gmail.com).
-    [d,k] = size(mu);
-
-    if size(sigma,1)==d && size(sigma,2)==d && k==1
-        X = bsxfun(@minus,X,mu);
-        [R,p]= chol(sigma);
-        if p ~= 0
-            error('ERROR: sigma is not PD.');
-        end
-        Q = R'\X;
-        q = dot(Q,Q,1);  % quadratic term (M distance)
-        c = d*log(2*pi)+2*sum(log(diag(R)));   % normalization constant
-        y = -(c+q)/2;
-    elseif size(sigma,1)==d && size(sigma,2)==k
-        lambda = 1./sigma;
-        ml = mu.*lambda;
-        q = bsxfun(@plus,X'.^2*lambda-2*X'*ml,dot(mu,ml,1)); % M distance
-        c = (d*log(2*pi)+sum(log(sigma),1))/(-2); % normalization constant
-        y = bsxfun(@plus,q/(-2),c);
-    elseif size(sigma,1)==1 && size(sigma,2)==k
-        X2 = repmat(dot(X,X,1)',1,k);
-        D = bsxfun(@plus,X2-2*X'*mu,dot(mu,mu,1));
-        q = bsxfun(@times,D,1./sigma);  % M distance
-        c = d*log(2*pi*sigma)/(-2);          % normalization constant
-        y = bsxfun(@plus,q/(-2),c);
-    else
-        error('Parameters mismatched.');
-    end
-end
--- q2.m
+++ q2.m
@@ -6,7 +6,7 @@ NUM_DATA = size(Spikes, 2);
 DIMENSION = size(params.mu, 1);
 
 % Initializing and plotting all the spikes
-N_Iter = 5;
+N_Iter = 100;
 N_k = zeros(1, NUM_CLUSTERS);
 likelihood = zeros(1, N_Iter);
 covvar = repmat(params.Sigma, [1,1,NUM_CLUSTERS]);
@@ -19,38 +19,25 @@ figure;
 for k=1:N_Iter
     % E Step : Choose between mvnpdf and custom implementation
     r = zeros(NUM_CLUSTERS, NUM_DATA);
-%     for i=1:NUM_CLUSTERS
-%         for j=1:NUM_DATA
-%              r(i,j) = exp(logmvnpdf(Spikes(:,j),params.mu(:,i), ...
-%                       covvar(:,:,i), params.pi(i)) - ... 
-%                       boxed_term(Spikes(:,j), params, covvar));            
-%         end
-%     end
-
     for i=1:NUM_DATA
         bt = boxed_term(Spikes(:,i), params, covvar);
         for j=1:NUM_CLUSTERS
              r(j,i) = exp(logmvnpdf(Spikes(:,i), params.mu(:,j), ...
-                 covvar(:,:,j), params.pi(j)));
+                 covvar(:,:,j), params.pi(j)) - bt);
         end
     end
-    
-    r_sum = sum(r);
-%     for i=1:NUM_CLUSTERS
-%         r(i,:) = r(i,:)./r_sum;  
-%     end
-    r_log = -log(r);
-
-
+   
     % M Step
     for i=1:NUM_CLUSTERS
-        mean_sum = 0;
-        N_k = zeros(1, NUM_CLUSTERS);
+         N_k(i) = sum(r(i,:));
+    end
+    for i=1:NUM_CLUSTERS
+        mean_sum = 0;      
         for j=1:NUM_DATA
             mean_sum = mean_sum + r(i,j).* Spikes(:,j);
-            N_k(i) = N_k(i) + r(i,j);
         end
         params.mu(:,i) = mean_sum ./ N_k(i);
+        params.pi(i) = N_k(i) / NUM_DATA;
     end
 
     for i=1:NUM_CLUSTERS
@@ -58,23 +45,11 @@ for k=1:N_Iter
         for j=1:NUM_DATA
             cov_sum = cov_sum + r(i,j).*((Spikes(:,j) - params.mu(:,i)) * (Spikes(:,j) - params.mu(:,i))');
         end
-        covvar(:,:,i) = cov_sum;
+        covvar(:,:,i) = cov_sum ./ N_k(i);
     end
 
     % Evaluation log likelihood
     L = 0;
-%     for j =1:NUM_DATA
-%         L_this = 0;
-%         for i = 1:NUM_CLUSTERS
-% %             L_this = L_this + mvnpdf(Spikes(:,i), params.mu(:,j), ...
-% %                 covvar(:,:,j)) * params.pi(j);
-%             L_this = L_this + exp(logmvnpdf(Spikes(:,j),params.mu(:,i), ...
-%                       covvar(:,:,i), params.pi(i))- ... 
-%                       boxed_term(Spikes(:,j), params, covvar)); 
-%         end
-%         L = L + L_this;
-%     end
-
     for i=1:NUM_DATA
         L = L + boxed_term(Spikes(:,i), params, covvar);
     end
